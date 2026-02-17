560514be5c57ad54e332029d4c76b8d8841b03e3
Sok Pyeon <leland@candlstrategy.com>
2026-02-16 20:20:57 -0600

    Add sitemap, robots.txt, SEO config, fix deck

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  trailingSlash: true,
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          { key: 'X-robots-tag', value: 'index, follow' }
+        ]
+      }
+    ]
+  }
 };
 
 export default nextConfig;
